67a6a993cbcbc36286b980fb1282a7d8451c49ea
Morgan Batterham <morgan@morganbatterham.me>
2020-08-13 10:45:12 +1000

    basic routing

--- server.js
+++ server.js
@@ -3,19 +3,31 @@ const fs = require('fs');
 const { red } = require('color-name');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    console.log(req.method, req.url);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        default:
+            path += '404.html'
+            break
+    }
+
     // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
         } else {
-            res.write(data);
-            res.end();
+            res.end(data);
         }
     })
 

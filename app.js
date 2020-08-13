1477fd2457975ff11585a8900de47e29ba14eb67
Morgan Batterham <morgan@morganbatterham.me>
2020-08-13 11:31:17 +1000

    basic about page

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-
     res.send('<p>home page</p>');
+});
 
-})
+app.get('/about', (req, res) => {
+    res.send('<p>about page</p>');
+});

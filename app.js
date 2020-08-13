255810eeaca7a33f7649605fc9a395d866f1607b
Morgan Batterham <morgan@morganbatterham.me>
2020-08-13 13:57:12 +1000

    index view

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews')
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index')
 });
 
 app.get('/about', (req, res) => {

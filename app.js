4a3770f59f1bd46016384c3c58cbe5d3e5cfd978
Morgan Batterham <morgan@morganbatterham.me>
2020-08-13 11:50:42 +1000

    404 page and all routes

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.get('/about', (req, res) => {
 // redirects
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
-}
+});
 
 // 404 page
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname })
+});
